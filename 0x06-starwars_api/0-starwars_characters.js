73ec46f07386ad684f1f64f8ad332c244212bcd6
majidied <majidimajidi2003@gmail.com>
2024-07-14 03:31:12 +0100

    feat: Refactor Star Wars character retrieval script for improved code readability and efficiency

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
-/**
- * The URL for the Star Wars API endpoint.
- * @type {string}
- */
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const util = require('util');
+const requestPromise = util.promisify(request);
 
-request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    for (const character of data.characters) {
-      request(character, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          const data = JSON.parse(body);
-          console.log(data.name);
-        }
-      });
-    }
+async function fetchFilmCharacters (filmId) {
+  const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+  try {
+    const filmResponse = await requestPromise(url);
+    const filmData = JSON.parse(filmResponse.body);
+
+    const characterPromises = filmData.characters.map(async (characterUrl) => {
+      const characterResponse = await requestPromise(characterUrl);
+      return JSON.parse(characterResponse.body).name;
+    });
+
+    const characterNames = await Promise.all(characterPromises);
+    characterNames.forEach(name => console.log(name));
+  } catch (error) {
+    console.error(`Failed to fetch data: ${error.message}`);
   }
-});
+}
+
+const filmId = process.argv[2];
+fetchFilmCharacters(filmId);

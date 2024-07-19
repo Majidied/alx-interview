4849bd93ed4719169c1517a9e310aef5538bf4e6
majidied <majidimajidi2003@gmail.com>
2024-07-19 01:29:10 +0100

    Refactor rotate_2d_matrix function for improved code readability and efficiency

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -14,13 +14,13 @@ def rotate_2d_matrix(matrix):
     Returns:
         None: The matrix is rotated in-place.
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-
-    np.transpose(matrix)
-    for row in matrix:
-        row.reverse()
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[x][i]
+            matrix[x][i] = matrix[y][x]
+            matrix[y][x] = matrix[j][y]
+            matrix[j][y] = tmp

7a8173e20b21e3196410343528b4244a1129c66a
majidied <majidimajidi2003@gmail.com>
2024-07-19 01:44:00 +0100

    Refactor rotate_2d_matrix function for improved code readability and efficiency

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,24 +5,20 @@
 def rotate_2d_matrix(matrix):
     """Rotates an m by n 2D matrix in place.
     """
-    if not isinstance(matrix, list):
+    if not isinstance(
+        matrix,
+        list) or len(matrix) == 0 or not all(
+        isinstance(
+            row,
+            list) for row in matrix):
         return
-    if len(matrix) <= 0:
+    if not all(len(row) == len(matrix) for row in matrix):
         return
-    if not all(map(lambda x: isinstance(x, list), matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for row in matrix:
+        row.reverse()

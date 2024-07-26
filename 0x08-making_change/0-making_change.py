42ac3a2dd295ff992c97602a296233a382a3c320
majidied <majidimajidi2003@gmail.com>
2024-07-26 02:24:42 +0100

    Refactor makeChange function for improved code readability and efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,14 +4,17 @@
 
 def makeChange(coins, total):
     """
-    Determines the minimum number of coins needed to meet a given amount of change.
+    Determines the minimum number of coins needed to meet a
+    given amount of change.
 
     Parameters:
-    coins (List[int]): A list of integers representing the available coin denominations.
+    coins (List[int]): A list of integers representing the available
+    coin denominations.
     total (int): The total amount of change needed.
 
     Returns:
-    int: The minimum number of coins needed to make the change, or -1 if it's not possible.
+    int: The minimum number of coins needed to make the change,
+    or -1 if it's not possible.
     """
     if total <= 0:
         return 0

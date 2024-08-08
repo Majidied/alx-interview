19f3c07dfa11fe4300b4a25e97512252e90921ac
majidied <majidimajidi2003@gmail.com>
2024-08-08 23:48:12 +0100

    Refactor prime game logic for improved code readability and efficiency

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,47 +2,41 @@
 """ Prime Game """
 
 
-def is_prime(n):
-    """ Check if a number is prime """
-    if n < 2:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-
-def calculate_primes(n, primes):
-    """ Calculate primes """
-    if n <= 1:
-        return 0
-    if primes[n] == 1:
-        return 1
-    if primes[n] == 0:
-        return 0
-    if is_prime(n):
-        primes[n] = 1
-        return 1
-    primes[n] = 0
-    return 0
-
-
 def isWinner(x, nums):
-    """ Prime Game """
-    primes = [2 for _ in range(max(nums) + 1)]
-    calculate_primes(max(nums), primes)
-    maria = 0
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+
     ben = 0
-    for n in nums:
-        count = 0
-        for i in range(2, n + 1):
-            count += calculate_primes(i, primes)
-        if count % 2 == 0:
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
             ben += 1
         else:
             maria += 1
-    if maria < ben:
+    if ben > maria:
         return "Ben"
     if maria > ben:
         return "Maria"
     return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
